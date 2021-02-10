2982b2193c5c968288cf54be26b2cd3c14351b4b
slowlove729 <slowlove729@naver.com>
2021-02-10 20:47:11 +0900

    TREE

---
+++ TREE/linked.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct linked
+{
+    int num;
+    struct linked *next;
+}list;
+
+void addlist(list *ls, int num);
+
+int main(void)
+{
+    list *start = malloc(sizeof(list)); //리스트의 시작점
+    start->next = NULL;
+
+    printf("프로그램 시작");
+    addlist(start, 1);
+    printf("OK 1");
+    addlist(start, 2);
+    printf("OK 2");
+    addlist(start, 3);
+    printf("OK 3");
+    addlist(start, 4);
+    printf("OK 4");
+
+    list *curr = malloc(sizeof(list));
+    curr = start->next;
+
+    while (curr != NULL)
+    {
+        printf("%d\n", curr->num);
+        curr = curr->next;
+    }
+
+
+
+    return 0;
+}
+
+
+
+void addlist(list *ls, int num)
+{
+    if (ls->next != NULL)
+    {
+        printf("addlist if");
+        addlist(ls->next, num);
+    }
+    else if (ls->next == NULL)
+    {
+        printf("addlist else if");
+        list *n = malloc(sizeof(list));
+        n->num = num;
+        ls->next = n;
+    }
+}
--- TREE/tree.c
+++ TREE/tree.c
@@ -1,14 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// tree 자료형 선언 
 typedef struct TREE
 {
     int number;
-    struct TREE *next;
+    struct TREE *up; //큰 수의 주소를 추가할 변수
+    struct TREE *down; //작은 수의 주소를 추가할 변수
 }tree;
+
+addNumber(tree *list, int num);
 
 
 int main(void)
 {
-    //tree 자료 구조 만들기
+    tree *start;
+
+    addNumber(start, 1);
+
+}
+
+
+
+addNumber(tree *list, int num)
+{
+    if (list->number == NULL)
+    {
+        list->number = num;
+    }
+    else
+    {
+        if (list->number > num)
+        {
+            tree *n = malloc(sizeof(tree));
+            list->down = num;
+        }
+        else if (list->number < num)
+        {
+            tree *n = malloc(sizeof(tree));
+            list->up = num;
+        }
+        else 
+        {
+            printf("같은 수가 이미 있습니다.");
+        }
+    }
 }
